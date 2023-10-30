d289e2afbfbe319ebe398c8a3db7f59514a1f304
dazdaz <darren@dazdaz.org>
2023-10-30 20:26:25 +0100

    Update bazel.sh

--- bazel.sh
+++ bazel.sh
@@ -12,7 +12,7 @@ INFO: Elapsed time: 130.010s, Critical Path: 6.54s
 INFO: 113 processes: 46 internal, 67 linux-sandbox.
 INFO: Build completed successfully, 113 total actions
 
-daev@cloudshell:~/cloud_build/src/distroless (daev-playground)$ bazel build //java:java_base_root_amd64_debian12
+bazel build //java:java_base_root_amd64_debian12
 INFO: Analyzed target //java:java_base_root_amd64_debian12 (0 packages loaded, 0 targets configured).
 INFO: Found 1 target...
 Target //java:java_base_root_amd64_debian12 up-to-date:
